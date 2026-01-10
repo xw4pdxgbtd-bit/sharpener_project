4cd73384ee6d5f6cc22bcb9fb0ae49aa96881900
Thriven Komarr <thrivenkomarr@gmail.com>
2026-01-09 18:50:57 -0800

    createElements_DomRelations

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-const mainheading = document.querySelector('#main-heading');
-mainheading.style.textAlign = 'right';
-
-const basketheading = document.querySelector('#basket-heading');
-basketheading.style.color = 'brown';
-
+const para = document.createElement('p');
+const paraText = document.createTextNode('Total Fruits:4');
+para.appendChild(paraText);
+const divs = document.getElementsByTagName('div');
+const secondDiv = divs[1];
+secondDiv.appendChild(para);
 const fruits = document.querySelector('.fruits');
-fruits.style.backgroundColor = 'gray';
-fruits.style.padding = '30px';
-fruits.style.margin = '30px';
-fruits.style.width = '50%';
-fruits.style.borderRadius = '5px';
-fruits.style.listStyleType = 'None';
+secondDiv.insertBefore(para, fruits);
 
-const fruit = document.querySelectorAll('.fruit');
-for (let i = 0; i < fruit.length; i++){
-    fruit[i].style.backgroundColor = 'white';
-    fruit[i].style.padding = '10px';
-    fruit[i].style.margin = '10px';
-    fruit[i].style.borderRadius = '5px';
-}
-
-const evenfruit = document.querySelectorAll('.fruit:nth-child(even)');
-for (let i = 0; i < evenfruit.length; i++){
-    evenfruit[i].style.backgroundColor = 'brown';
-    evenfruit[i].style.color = 'white';
-
-}
+const head = document.createElement('h3');
+const heading = document.createTextNode('Buy high quality organic fruits online');
+head.style.fontStyle = 'italic';
+head.appendChild(heading);
+const firstDiv = divs[0];
+firstDiv.appendChild(head);
+para.id = 'fruits-total';
